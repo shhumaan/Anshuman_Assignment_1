05cf2f0130283a67acab42d77ed1c5b5ec7f9125
shhumaan <48822497+shhumaan@users.noreply.github.com>
2023-10-26 01:00:11 -0400

    init

---
+++ Anshuman_Assignment_1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Anshuman_Assignment_1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
